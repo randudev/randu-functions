55148acebf22947dcc62a26af72d5960b0542162
randudev <admin@randu.mx>
2025-01-21 11:24:11 -0600

    Create ml_functions.R

---
+++ ml_functions.R
@@ -0,0 +1,128 @@
+library(lubridate)
+library(httr2)
+
+#source("~/airtable_utils.R")
+
+get_active_token <- function(){
+  tkdata <- airtable_getrecorddata_byid("rec7opQaYsHnpRhgb", "tokens", Sys.getenv('AIRTABLE_DEV_BASE'))
+  tkdata <- tkdata$fields
+  token_expires <- lubridate::as_datetime(tkdata$token_expires)
+  refresh_token <- tkdata$refresh_token
+  access_token <- tkdata$access_token
+  
+  if(Sys.time()<token_expires){
+    validat <- access_token
+  }else{
+    request("https://api.mercadolibre.com/oauth/token") %>% 
+      req_method("POST") %>% 
+      req_headers(accept= "application/json") %>% 
+      req_headers('content-type' = 'application/x-www-form-urlencoded') %>% 
+      req_body_json(list('grant_type'='refresh_token',
+                         'client_id'=Sys.getenv('ML_APP_CLIENTID'),
+                         'client_secret'=Sys.getenv('ML_APP_CLIENTSECRET'),
+                         'refresh_token'=refresh_token)) %>% 
+      req_perform()
+    
+    newtokendata <- last_response() %>% resp_body_json()
+    newaccess_token <- newtokendata$access_token
+    newrefreshtoken <- newtokendata$refresh_token
+    newexpire <- last_response() %>% resp_date() + 21600 
+    
+    airtable_updatesinglerecord(fieldslist = list('access_token'=newaccess_token,
+                                                  'refresh_token'=newrefreshtoken,
+                                                  'token_expires'=newexpire), 
+                                tablename = "tokens", base_id = Sys.getenv('AIRTABLE_DEV_BASE'),
+                                recordid = "rec7opQaYsHnpRhgb")
+    validat <- newaccess_token
+  }
+  validat
+}
+
+get_mlorder_byid <- function(orderid, mltoken){
+  request(paste0("https://api.mercadolibre.com/orders/",orderid)) %>% 
+    req_auth_bearer_token(ml_token) %>% 
+    req_headers(accept= "application/json") %>% 
+    req_headers('content-type' = 'application/x-www-form-urlencoded') %>% 
+    req_perform() %>% 
+    resp_body_json()
+}
+
+
+register_mlorder_in_airtable <- function(mlorder, ml_token){
+  lineitems_recordid <- register_lineitems_ml(mlorder, ml_token)
+  #client_recordid <- register_client(shopifyorder)
+  #shippingaddress_recordid <- register_address(shopifyorder)
+  fieldslist <- list(
+    'fecha'=mlorder$date_created,
+    'canal_venta'='mercadolibrernd',
+    'ventas_producto'=lineitems_recordid,
+    'id_origen'=as.character(mlorder$id)
+  )
+  if(!is.null(mlorder$pack_id)){
+    fieldslist <- append(fieldslist,list('ml_packid'=as.character(mlorder$pack_id)))
+  }
+  newov_content  <- airtable_createrecord(fieldslist, "ordenes_venta", Sys.getenv('AIRTABLE_CES_BASE'))
+}
+
+#get_shipmentdetails
+mlorder_checkfullfilment <- function(mlorder, ml_token){
+  resp <- "order_without_shippingid"
+  shipid <- mlorder$shipping$id
+  if(!is.null(shipid)){
+    request(paste0("https://api.mercadolibre.com/shipments/",shipid)) %>% 
+      req_auth_bearer_token(ml_token) %>% 
+      req_headers(accept= "application/json") %>% 
+      req_headers('content-type' = 'application/x-www-form-urlencoded') %>% 
+      req_perform()
+    
+    shipment_details <- last_response() %>% resp_body_json()
+    resp <- shipment_details$logistic_type 
+    if(is.null(resp)){
+      resp <- "seller_fulfilled"
+    }
+  }
+  resp
+}
+
+register_lineitems_ml <- function(mlorder, ml_token){
+  vp_recordidlist <- vector(mode="list", 1)
+  
+  cantidad <- as.double(mlorder$order_items[[1]]$quantity)
+  nombre_producto <- mlorder$order_items[[1]]$item$title
+  precio <- as.double(mlorder$order_items[[1]]$unit_price)
+  sku <- mlorder$order_items[[1]]$item$seller_sku
+  comentarios <- ""
+  fullfilment_type <- mlorder_checkfullfilment(mlorder, ml_token)
+  if(fullfilment_type=="fulfillment"){
+    fieldslist <- list(
+      'cantidad'=cantidad,
+      'helper_product_name'=nombre_producto,
+      'precio_unitario'=precio,
+      'pendiente_envio'=0,
+      'vp_revisada'=TRUE,
+      'comentarios'=comentarios)
+  }else{
+    fieldslist <- list(
+      'cantidad'=cantidad,
+      'helper_product_name'=nombre_producto,
+      'precio_unitario'=precio,
+      'pendiente_envio'=cantidad,
+      'comentarios'=comentarios) 
+  }
+  
+  if(!is.null(sku) && str_detect(sku,"^\\d\\d\\d\\d\\d$") ){
+    product_recordid_list <- airtable_getrecordslist("productos",Sys.getenv('AIRTABLE_CES_BASE'), 
+                                                     formula=paste0("sku=",sku))
+    
+    recordid_producto <- list(producto=list(product_recordid_list[[1]]$id))
+    fieldslist <- append(fieldslist, recordid_producto)
+  }
+  newvp_content  <- airtable_createrecord(fieldslist, "ventas_producto", Sys.getenv('AIRTABLE_CES_BASE'))
+  if(!is.null(newvp_content)){
+    vp_recordidlist[[1]] <- newvp_content$id[[1]]
+  }else{
+    print(paste0("hubo un problema al registrar la el line_item 
+                   (venta_producto #"))
+  }
+  vp_recordidlist
+}
