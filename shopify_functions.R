aff893de6e0e53eed1c8452d6e1a788c1863a6bc
Arturo Huerta <89116386+Arthured@users.noreply.github.com>
2025-03-14 11:58:19 -0600

    Update shopify_functions.R

--- shopify_functions.R
+++ shopify_functions.R
@@ -305,3 +305,139 @@ register_shopifyorder_in_airtablev2 <- function(shopifyorder){
   )
   newov_content  <- airtable_createrecord(fieldslist, "ordenes_venta", Sys.getenv('AIRTABLE_CES_BASE'))
 }
+
+consulta_por_nombre <- function(order_name,access_token) {
+  shopify_url <- "https://randumexico.myshopify.com/admin/api/2025-01/graphql.json"
+  # Consulta GraphQL para obtener los detalles de la orden por nombre usando paste0
+  query <- paste0('
+  {
+    orders(first: 10, query: "name:', order_name, '") {
+      edges {
+        node {
+          id
+          name
+          fulfillmentOrders(first: 10) {
+            edges {
+              node {
+                id
+                status
+                lineItems(first: 5) {
+                  edges {
+                    node {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          }
+          lineItems(first: 10)  {
+              edges {
+                node{
+                  id
+                  name
+                }
+              }
+            }
+        }
+      }
+    }
+  }
+  ')
+  
+  # Realizar la solicitud GraphQL
+  response <- request(shopify_url) %>%
+    req_headers('X-Shopify-Access-Token' =access_token,
+                "Content-Type" = "application/json") %>%
+    req_body_json(list(query = query)) %>%
+    req_perform()
+  
+  # Parsear la respuesta JSON
+  respuesta <- resp_body_json(response)
+  
+  return(respuesta)
+}
+
+mandar_numero_rastreo <- function(numeros_rastreo,paqueteria,fulfill_id,access_token){
+  #shopify_url <- 
+  shopify_url <- "https://randumexico.myshopify.com/admin/api/2025-01/graphql.json"
+  if(paqueteria=="PAQUETEXPRESS"){
+    url <- "https://www.paquetexpress.com.mx/"
+    paqueteria <- "Other"
+  }else{
+    url <- NULL
+  }
+  fulfillment_data <- list(
+    lineItemsByFulfillmentOrder = list(
+      list(
+        fulfillmentOrderId = fulfill_id
+      )
+    ),
+    #orderId=order_id,
+    notifyCustomer = TRUE,
+    trackingInfo = list(
+      company = paqueteria,
+      number = numeros_rastreo[[1]]
+    )
+  )
+  if(!is.null(url)){
+    fulfillment_data$trackingInfo$url <- url
+  }
+  if(length(numeros_rastreo)>1){
+    fulfillment_data$trackingInfo$numbers <- numeros_rastreo[-1]
+  }
+  
+  variables <- list(
+    fulfillment = fulfillment_data,
+    message = "RanduMx"
+  )
+  
+  mutation <- '
+  mutation fulfillmentCreate($fulfillment: FulfillmentInput!, $message: String) {
+  fulfillmentCreate(fulfillment: $fulfillment, message: $message) {
+    fulfillment {
+      id
+    }
+    userErrors {
+      field
+      message
+    }
+  }
+  }'
+  
+  response <- request(shopify_url) %>%
+    req_method("POST") %>% 
+    req_headers('X-Shopify-Access-Token' =access_token,
+                "Content-Type" = "application/json") %>%
+    req_body_json(list(query = mutation,variables = variables)) %>%
+    req_perform() %>% 
+    resp_body_json()
+  return(response)
+}
+
+enviar_nota <- function(order_id,numeros_rastreo,paqueteria,access_token){
+  shopify_url <- "https://randumexico.myshopify.com/admin/api/2025-01/graphql.json"
+  notes <- paste("Paqueteria:",paqueteria,"\nNúmeros de rastreo adicionales:\n", paste(numeros_rastreo, collapse = "\n"))
+  
+  notes_mutation <- paste0('
+    mutation {
+      orderUpdate(input: {
+        id: "', order_id, '",
+        note: "', notes, '"
+      }) {
+        order {
+          id
+        }
+      }
+    }
+    ')
+  
+  # Realizar la solicitud para actualizar las notas
+  response_notes <- request(shopify_url) %>%
+    req_headers('X-Shopify-Access-Token' =access_token,
+                "Content-Type" = "application/json") %>%
+    req_body_json(list(query = notes_mutation)) %>%
+    req_perform() %>% 
+    resp_body_json()
+  return(response_notes)
+}
