a6bf4ba38dadf8628a19ad1467ec101df3cc633e
Arturo Huerta <89116386+Arthured@users.noreply.github.com>
2025-10-01 12:00:40 -0600

    Update archivos_functions.R

--- archivos_functions.R
+++ archivos_functions.R
@@ -4,8 +4,11 @@ paquetes <- c("tidyr","emayili")
 cargar_paquetes(paquetes)
 
 
-procesar_gs1 <- function(id_archivo){
-  archivo <- airtable_getrecordslist("archivos",Sys.getenv("AIRTABLE_RIR_BASE"),paste0("id_archivo='",id_archivo,"'"))[[1]]
+procesar_gs1 <- function(id_archivo,archivo=NULL){
+  if(is.null(archivo)){
+    archivo <- airtable_getrecordslist("archivos",Sys.getenv("AIRTABLE_RIR_BASE"),paste0("id_archivo='",id_archivo,"'"))[[1]]
+  }
+  
   if(archivo$fields$tipo=="GS1"){
     data <- read.xlsx(archivo$fields$archivo[[1]]$url)
     if(length(data)==25){
@@ -13,6 +16,10 @@ procesar_gs1 <- function(id_archivo){
         
         producto <- airtable_getrecordslist("productos",Sys.getenv("AIRTABLE_RIR_BASE"),
                                             paste0("sku=",data[i,]$Código.Interno),c("sku"))[[1]]
+        if(length(producto)==0){
+          print(paste0("No se encontro el producto ", data[i,]$Código.Interno))
+          next
+        }
         #producto <- airtable_getrecordslist("productos",Sys.getenv("AIRTABLE_CES_BASE"),paste0("sku=",data[i,]$Código.Interno),c("sku"))[[1]]
         fields <- list(
           'gs1_code' =paste0(data[i,]$Consecutivo,data[i,]$Dígito.Verificador),
@@ -27,3 +34,14 @@ procesar_gs1 <- function(id_archivo){
   }
   
 } 
+
+archivos <- function(){
+  archivos_pendientes <- airtable_getrecordslist("archivos",Sys.getenv("AIRTABLE_RIR_BASE"),"Status='Pendiente'")
+  if(length(archivos)!=0){
+    for(archivo in archivos_pendientes){
+      if(archivo$fields$tipo=='GS1'){
+        procesar_gs1(archivo$fields$id_archivo,archivo)
+      }
+    }
+  }
+}
