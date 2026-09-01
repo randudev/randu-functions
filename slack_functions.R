0d6b4567f5d093841bc8881660b41ec467b5cb81
Arturo Huerta <89116386+Arthured@users.noreply.github.com>
2026-09-01 10:08:55 -0600

    Update slack_functions.R

--- slack_functions.R
+++ slack_functions.R
@@ -111,7 +111,7 @@ slack_npu <- function(cuerpo,ml_token){
     if(last_response()$status_code %in% c(199:299)){
       Sys.sleep(10)
       item <- ml_status_item(resp$id,ml_token, "active")
-      
+      #item <- ml_stock_item(item_ml$id,ml_token,"5")
       ml_crear_descripcion(resp$id, titulo,ml_token)
       mensaje <- paste0(item$id,": ",item$permalink)
       tryCatch(expr={
@@ -641,7 +641,8 @@ slack_status_publi <- function(cuerpo,ml_token,amz_token){
                       }
                     }
                     if(item_ml$status=="paused"){
-                      ml_status_item(item_ml$id,ml_token,"active")
+                      #ml_status_item(item_ml$id,ml_token,"active")
+                      item <- ml_stock_item(item_ml$id,ml_token,"0")
                       if(!last_response()$status_code %in% c(199:299)){
                         mensaje_ml <- paste0("Ocurrio un error al pausar el item: ",item_amz$id,"\nError: ",
                                              last_response()$status_code,"\n Body: ",last_response() %>% resp_body_string())
@@ -704,6 +705,7 @@ slack_status_publi <- function(cuerpo,ml_token,amz_token){
                   if(!is.null(item_ml$status)){
                     if(item_ml$status=="paused"){
                       ml_status_item(item_ml$id,ml_token,"active")
+                      item <- ml_stock_item(item_ml$id,ml_token,"0")
                       if(!last_response()$status_code %in% c(199:299)){
                         mensaje_ml <- paste0("Ocurrio un error al pausar el item: ",item_amz$id,"\nError: ",
                                              last_response()$status_code,"\n Body: ",last_response() %>% resp_body_string())
@@ -750,7 +752,8 @@ slack_status_publi <- function(cuerpo,ml_token,amz_token){
                       }
                     }
                     if(item_ml$status=="paused"){
-                      ml_status_item(item_ml$id,ml_token,"active")
+                      #ml_status_item(item_ml$id,ml_token,"active")
+                      item <- ml_stock_item(item_ml$id,ml_token,"0")
                       if(!last_response()$status_code %in% c(199:299)){
                         mensaje_ml <- paste0("Ocurrio un error al pausar el item: ",item_amz$id,"\nError: ",
                                              last_response()$status_code,"\n Body: ",last_response() %>% resp_body_string())
@@ -902,7 +905,8 @@ slack_status_publi <- function(cuerpo,ml_token,amz_token){
                   item_ml <- ml_obtener_item(publi_ml$fields$id_canal,ml_token)
                   if(!is.null(item_ml$status)){
                     if(item_ml$status=="active"){
-                      ml_status_item(item_ml$id,ml_token,"paused")
+                      #ml_status_item(item_ml$id,ml_token,"paused")
+                      item <- ml_stock_item(item_ml$id,ml_token,"0")
                       if(!last_response()$status_code %in% c(199:299)){
                         mensaje_ml <- paste0("Ocurrio un error al pausar el item: ",item_amz$id,"\nError: ",
                                              last_response()$status_code,"\n Body: ",last_response() %>% resp_body_string())
